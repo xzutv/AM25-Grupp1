3a85da8502a6f2a4fc8d1ad0076f4e95305682f0
Haimen <haimenabbas@gmail.com>
2025-03-10 10:29:04 +0100

    added pngs and classes

---
+++ core/src/main/java/se/yrgo/AM25_grupp1/Input.java
@@ -0,0 +1,61 @@
+package se.yrgo.AM25_grupp1;
+
+import com.badlogic.gdx.*;
+
+public class Input implements InputProcessor {
+
+    @Override
+    public boolean keyDown(int keycode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyDown'");
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyUp'");
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchDown'");
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchUp'");
+    }
+
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchCancelled'");
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'touchDragged'");
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseMoved'");
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'scrolled'");
+    }
+    
+}
