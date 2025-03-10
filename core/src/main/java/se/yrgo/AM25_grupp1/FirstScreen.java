ff4d8bead3c97a070ee35579cface90fa96deed9
AlrikM <alrbri0804@skola.goteborg.se>
2025-03-10 11:41:23 +0100

    Changed main and firstcreen for texture

--- core/src/main/java/se/yrgo/AM25_grupp1/FirstScreen.java
+++ core/src/main/java/se/yrgo/AM25_grupp1/FirstScreen.java
@@ -1,12 +1,40 @@
 package se.yrgo.AM25_grupp1;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Align;
+import org.w3c.dom.css.Rect;
+
+import java.awt.*;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private Texture texture;
+    private Rectangle rectangle;
+    private Main main;
+
+    public FirstScreen(Main main){
+        this.main = main;
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        rectangle = new Rectangle(0,0, 50,40);
+        texture = new Texture("bucket.png");
+    }
+
+
     @Override
     public void show() {
         // Prepare your screen here.
+        final int width = Gdx.graphics.getWidth();
+        final int height = Gdx.graphics.getHeight();
+
+        rectangle.setLocation(100, 0);
     }
 
     @Override
