0cf6f1720665bf0688e38196b3ac1a9df95cd7fa
Najib Bardash <najib.bardash@gmail.com>
2025-03-10 12:56:50 +0100

    Adding character graphics and hitbox

--- core/src/main/java/se/yrgo/AM25_grupp1/FirstScreen.java
+++ core/src/main/java/se/yrgo/AM25_grupp1/FirstScreen.java
@@ -1,22 +1,85 @@
 package se.yrgo.AM25_grupp1;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import java.util.ArrayList;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+    Main main;
+    Texture charTexture;
+    Texture backgroundTexture;
+
+    Sprite charSprite;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
+    Rectangle charRectangle;
+
+
+
+    public FirstScreen(Main main) {
+        this.main = main;
+        charTexture = new Texture("character.png");
+        backgroundTexture = new Texture("background2.png");
+        charSprite = new Sprite(charTexture);
+        charSprite.setSize(1, 1);
+        charSprite.setPosition(3, 3);
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
+        charRectangle = new Rectangle();
+    }
+
     @Override
     public void show() {
-        // Prepare your screen here.
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        logic();
+        draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        // Resize your screen here. The parameters represent the new window size.
+        viewport.update(width, height, true); // true centers the camera
+    }
+
+    private void logic() {
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        float charWidth = charSprite.getWidth();
+        float charHeight = charSprite.getHeight();
+
+        charSprite.setX(MathUtils.clamp(charSprite.getX(), 0, worldWidth - charWidth));
+
+        float delta = Gdx.graphics.getDeltaTime();
+        charRectangle.set(charSprite.getX(), charSprite.getY(), charWidth, charHeight);
+    }
+
+        private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        charSprite.draw(spriteBatch);
+
+        spriteBatch.end();
     }
 
     @Override
--- core/src/main/java/se/yrgo/AM25_grupp1/Main.java
+++ core/src/main/java/se/yrgo/AM25_grupp1/Main.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    private FirstScreen firstScreen;
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new FirstScreen(this));
     }
 }
