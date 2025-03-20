0bedb943321a99204b2f59fd491e5fbeb0afc71e
Najib Bardash <najib.bardash@gmail.com>
2025-03-20 11:15:02 +0100

    Initiating project of refactoring

--- core/src/main/java/se/yrgo/AM25_grupp1/Main.java
+++ core/src/main/java/se/yrgo/AM25_grupp1/Main.java
@@ -5,7 +5,10 @@ import com.badlogic.gdx.Game;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     private FirstScreen firstScreen;
+    private GameScreen gameScreen;
+
     private int sessionHighscore;
+
     @Override
     public void create() {
         setScreen(new FirstScreen(this));
@@ -17,5 +20,9 @@ public class Main extends Game {
 
     public void setSessionHighscore(int sessionHighscore) {
         this.sessionHighscore = sessionHighscore;
+    }
+
+    public void startGame() {
+        setScreen(gameScreen);
     }
 }
